3d50942d61356ffbc5a5813900dab7cf9bd31f42
Matteo <matteo.dinoia@edu.unito.it>
2023-03-21 19:59:38 +0100

    Card refactor (minimal)

--- src/obj/Card.java
+++ src/obj/Card.java
@@ -1,18 +1,17 @@
 package obj;
 
-
 import enumeration.*;
 
 public class Card {
-	
+
 	//CONSTRUCTOR  --------------------------------
 
 	public Card(CardValue cardNumber, CardSuit cardSuit) {
 		this.cardValue = cardNumber;
 		this.cardSuit = cardSuit;
 	}
-	
-	
+
+
 	//GETTER AND SETTER  --------------------------
 	private CardValue cardValue;
 	public CardValue getCardValue() {
@@ -26,42 +25,42 @@ public class Card {
 	public CardColor getCardColor() {
 		return cardSuit.getColor();
 	}
-	
-	private boolean hidden; 
+
+	private boolean hidden;
 	public boolean isHidden() {
 		return hidden;
 	}
 	public void setHidden(boolean hidden) {
 		this.hidden= hidden;
 	}
-	
+
 	//ABBREVIATION  -------------------------------
 	public String getAbbreviation() {
 		return getCardValue().getAbbreviation() + " " + getCardSuit().getAbbreviation();
 	}
-	
-	
+
+
 	//UTILITY METHODS  ----------------------------
 	public static boolean isCardStackableOnTop(Card bottom, Card top) {
 		if(top==null) return false;
-		
+
 		//IF nothing -> placable
 		if(bottom==null) return true;
-		
-		
+
+
 		//ELSE different color and under number
 		return !bottom.isSameColor(top) && top.isCardNextNumber(bottom);
 	}
 	public static boolean isCardNextInPiles(Card bottom, Card top) {
 		if(top==null) return false;
-		
+
 		//IF empty slot i can put it if ONE
 		if(bottom==null) return top.getCardValue()==CardValue.ONE;
-			
+
 		//ELSE same suit and next number
 		return bottom.isSameSuit(top) && bottom.isCardNextNumber(top);
 	}
-	
+
 	public boolean isSameColor(Card card) {
 		return card.getCardColor() == this.getCardColor();
 	}
