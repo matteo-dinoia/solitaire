5cfc5ef4b2dc5e4ee2abf6a73701d23d066dd8cb
Matteo <matteo.dinoia@edu.unito.it>
2023-04-07 11:24:53 +0200

    Remove unesed code and refactor constants

--- src/backend/Settings.java
+++ src/backend/Settings.java
@@ -1,18 +1,22 @@
 package backend;
 
 public class Settings {
-	public static final int NUM_COLS_TABLE=7,
-		NUM_SLOT_PILE=4;
-	public static final int WIDTH=100,
-		HEIGHT=(int)(1.6*WIDTH),
-		PARTIAL_HEIGHT=(int)(0.2*HEIGHT),
-		SPACE_BETWEEN=10;
+	public static final int NUM_COLS_TABLE = 7,
+		NUM_SLOT_PILE = 4;
+
+	//Graphics
+	public static double HOW_MUCH_SHOWN = 0.20;
+	public static int WIDTH = 130,
+		HEIGHT = (int)(1.6 * WIDTH),
+		PARTIAL_HEIGHT = (int)(HOW_MUCH_SHOWN * HEIGHT),
+		SPACE_BETWEEN = WIDTH / 10;
+	public static int WIN_W = WIDTH * NUM_COLS_TABLE + SPACE_BETWEEN * (NUM_COLS_TABLE + 1),
+		WIN_H = 12 * PARTIAL_HEIGHT + 2 * HEIGHT + 3 * SPACE_BETWEEN;
 
 	//Directory
-	public static final String IMG_PATH = "./../img/";
+	public static final String IMG_PATH = "../img/";
 
 	//User related
 	public static boolean onlyKing = false;
-	public static boolean aceInsetad = true;
 	public static String style = null;
 }
--- src/card_entity/CardValue.java
+++ src/card_entity/CardValue.java
@@ -3,7 +3,7 @@ package card_entity;
 public enum CardValue {
 
 	//VALUE  --------------------------------
-	ONE(1, "1"),
+	ONE(1, "A"),
 	TWO(2, "2"), THREE(3, "3"), FOUR(4, "4"), FIVE(5, "5"),
 	SIX(6, "6"), SEVEN(7, "7"), EIGHT(8, "8"), NINE(9, "9"), TEN(10, "10"),
 	JACK(11, "J"), QUEEN(12, "Q"), KING(13, "K");
@@ -20,9 +20,6 @@ public enum CardValue {
 
 	private String abbreviation;
 	public String getAbbreviation() {
-		if(this.getIntValue() == 1 && Settings.aceInsetad)
-			return "A";
-
 		return abbreviation;
 	}
 
